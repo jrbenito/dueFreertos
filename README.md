958ab7ccd2522737c47dd06033129b8a246eecd7
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2015-05-28 00:05:17 -0300

    FreeRTOS V8.2.1 demo converted to Due board

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dueFreertos
 This repository will experiment with Arduino Due board running FreeRTOS, the intend is not to generate code but to understand basic directory structure necessary (starting with Atmel ASF examples)
+
+# FreeRTOS v8.2.1 SAM3X-ek example converted to Due
+This branch has the FreeRTOS demo for Atmel's SAM3x development board converted to Due based on Atmels Due board configuration for FreeRTOS.
+
+Still missing: 
+  USB support (CDC for serial) because FreeRTOS does not use it by default
+  TODO: import from Atmel's ASF example
---
+++ config.mk
@@ -0,0 +1,177 @@
+#
+# Copyright (c) 2011 Atmel Corporation. All rights reserved.
+#
+# \asf_license_start
+#
+# \page License
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. The name of Atmel may not be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# 4. This software may only be redistributed and used in connection with an
+#    Atmel microcontroller product.
+#
+# THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+# EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# \asf_license_stop
+#
+
+# Path to top level Source code directory relative to this project directory.
+PRJ_PATH = ./src
+
+# Target CPU architecture: cortex-m3, cortex-m4
+ARCH = cortex-m3
+
+# Target part: none, sam3n4 or sam4l4aa
+PART = sam3x8e
+
+# Application target name. Given with suffix .a for library and .elf for a
+# standalone application.
+TARGET_FLASH = freertos_demo_flash.elf
+TARGET_SRAM = freertos_demo_sram.elf
+
+# List of C source files.
+CSRCS = \
+       asf/common/services/clock/sam3x/sysclk.c               \
+       asf/common/utils/interrupt/interrupt_sam_nvic.c        \
+       asf/sam/boards/arduino_due_x/init.c                    \
+       asf/sam/drivers/pio/pio.c                              \
+       asf/sam/drivers/pio/pio_handler.c                      \
+       asf/sam/drivers/pmc/pmc.c                              \
+       asf/sam/drivers/usart/usart.c                          \
+       asf/sam/utils/cmsis/sam3x/source/templates/exceptions.c \
+       asf/sam/utils/cmsis/sam3x/source/templates/gcc/startup_sam3x.c \
+       asf/sam/utils/cmsis/sam3x/source/templates/system_sam3x.c \
+       asf/thirdparty/FreeRTOS/list.c   \
+       asf/thirdparty/FreeRTOS/queue.c  \
+       asf/thirdparty/FreeRTOS/tasks.c  \
+       asf/thirdparty/FreeRTOS/timers.c \
+       asf/thirdparty/FreeRTOS/portable/GCC/ARM_CM3/port.c \
+       asf/thirdparty/FreeRTOS/portable/MemMang/heap_4.c \
+       Common-Demo-Source/BlockQ.c \
+       Common-Demo-Source/blocktim.c \
+       Common-Demo-Source/comtest.c \
+       Common-Demo-Source/countsem.c \
+       Common-Demo-Source/death.c \
+       Common-Demo-Source/dynamic.c \
+       Common-Demo-Source/flash_timer.c \
+       Common-Demo-Source/GenQTest.c \
+       Common-Demo-Source/integer.c \
+       Common-Demo-Source/PollQ.c \
+       Common-Demo-Source/recmutex.c \
+       Common-Demo-Source/semtest.c \
+       main.c \
+       main_blinky.c \
+       main_full.c \
+       ParTest.c \
+       serial.c 
+       
+
+# List of assembler source files.
+ASSRCS = 
+
+# List of include paths.
+INC_PATH = \
+       asf/common/boards                                      \
+       asf/common/services/clock                              \
+       asf/common/services/gpio                               \
+       asf/common/utils                                       \
+       asf/sam/boards                                         \
+       asf/sam/boards/arduino_due_x                           \
+       asf/sam/drivers/pio                                    \
+       asf/sam/drivers/pmc                                    \
+       asf/sam/drivers/usart                                  \
+       asf/sam/utils                                          \
+       asf/sam/utils/cmsis/sam3x/include                      \
+       asf/sam/utils/cmsis/sam3x/source/templates             \
+       asf/sam/utils/header_files                             \
+       asf/sam/utils/preprocessor                             \
+       asf/thirdparty/CMSIS/Include                           \
+       asf/thirdparty/CMSIS/Lib/GCC                           \
+       asf/thirdparty/FreeRTOS/include                        \
+       asf/thirdparty/FreeRTOS/portable/GCC/ARM_CM3           \
+       Common-Demo-Source/include        \
+       config \
+       ./ 
+
+# Additional search paths for libraries.
+LIB_PATH =  \
+       asf/thirdparty/CMSIS/Lib/GCC                          
+
+# List of libraries to use during linking.
+LIBS =  \
+       arm_cortexM3l_math                                 \
+       m                                                 
+
+# Path relative to top level directory pointing to a linker script.
+LINKER_SCRIPT_FLASH = asf/sam/utils/linker_scripts/sam3x/sam3x8/gcc/flash.ld
+LINKER_SCRIPT_SRAM  = asf/sam/utils/linker_scripts/sam3x/sam3x8/gcc/sram.ld
+
+# Path relative to top level directory pointing to a linker script.
+DEBUG_SCRIPT_FLASH = sam/boards/arduino_due_x/debug_scripts/gcc/arduino_due_x_flash.gdb
+DEBUG_SCRIPT_SRAM  = sam/boards/arduino_due_x/debug_scripts/gcc/arduino_due_x_sram.gdb
+
+# Project type parameter: all, sram or flash
+PROJECT_TYPE        = flash
+
+# Additional options for debugging. By default the common Makefile.in will
+# add -g3.
+DBGFLAGS = 
+
+# Application optimization used during compilation and linking:
+# -O0, -O1, -O2, -O3 or -Os
+OPTIMIZATION = -O1
+
+# Extra flags to use when archiving.
+ARFLAGS = 
+
+# Extra flags to use when assembling.
+ASFLAGS = 
+
+# Extra flags to use when compiling.
+CFLAGS = 
+
+# Extra flags to use when preprocessing.
+#
+# Preprocessor symbol definitions
+#   To add a definition use the format "-D name[=definition]".
+#   To cancel a definition use the format "-U name".
+#
+# The most relevant symbols to define for the preprocessor are:
+#   BOARD      Target board in use, see boards/board.h for a list.
+#   EXT_BOARD  Optional extension board in use, see boards/board.h for a list.
+CPPFLAGS = \
+       -D ARM_MATH_CM3=true                               \
+       -D BOARD=ARDUINO_DUE_X                             \
+       -D UDD_ENABLE                                      \
+       -D __FREERTOS__                                    \
+       -D __SAM3X8E__                                     \
+       -D printf=iprintf                                  \
+       -D scanf=iscanf
+
+# Extra flags to use when linking
+LDFLAGS = \
+
+# Pre- and post-build commands
+PREBUILD_CMD = 
+POSTBUILD_CMD = 
